355127bf67e0ac4e78f84c37ca8d51a53b83c17f
Nikos Zinas <zinas.nikos@gmail.com>
2015-05-06 17:12:53 +0200

    fixed jquery dependency

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ After that, run:
 gulp watch
 ```
 This will run a static server and launch the exercise. By default, the url is: http://localhost:3000 but can be configured.
+
+
+### Browser compatibility
+Latest Chrome only
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,7 @@ var
   Timeline = require('./components/timeline'),
   React = require('react'),
   mountNode = document.getElementById('app');
-  global.jQuery = require('../bower_components/jquery/dist/jquery.min');
+  global.jQuery = require('../bower_components/jquery/jquery.min');
   require('../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.min');
 
 
